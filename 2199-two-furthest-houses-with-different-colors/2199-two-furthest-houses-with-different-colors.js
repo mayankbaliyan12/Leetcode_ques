e020a07c082c7c63a3da79a5744ff9c4ba9cb144
mayankbaliyan12 <68837737+mayankbaliyan12@users.noreply.github.com>
2024-10-08 21:11:15 +0530

    Time: 53 ms (60.17%), Space: 49.3 MB (18.64%) - LeetHub

---
+++ 2199-two-furthest-houses-with-different-colors/2199-two-furthest-houses-with-different-colors.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} colors
+ * @return {number}
+ */
+var maxDistance = function(colors) {
+    let max=0;
+    for(let i=0;i<colors.length;i++){
+        for(let j=i+1;j<colors.length;j++){
+            if(colors[i]!==colors[j]){
+               max=Math.max(max,j-i)
+            }
+        }
+    }
+    return max;
+};

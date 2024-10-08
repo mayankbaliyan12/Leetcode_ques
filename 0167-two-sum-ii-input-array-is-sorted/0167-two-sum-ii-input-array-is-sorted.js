555f883633210fb3ab4ec2a2162ed9deb7c3cec4
mayankbaliyan12 <68837737+mayankbaliyan12@users.noreply.github.com>
2024-10-08 20:29:18 +0530

    Time: 56 ms (57.98%), Space: 50 MB (62.51%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let n=numbers.length;
+    let low=0;
+    let high=n-1;
+
+    while(low<high){
+        let sum=numbers[low]+numbers[high]
+        if(sum==target){
+             return [low+1,high+1];
+        }
+        if(sum>target){
+            high--;
+        }
+        else{
+            low++;
+        }
+    }
+};

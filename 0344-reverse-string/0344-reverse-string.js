51be75022a2511230d0740ed3c7482d86b74cd42
mayankbaliyan12 <68837737+mayankbaliyan12@users.noreply.github.com>
2024-10-08 20:37:54 +0530

    Time: 85 ms (22.6%), Space: 56.7 MB (65.11%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let low = 0;
+    let high = s.length - 1 ;
+
+    while(low < high){
+        const temp = s[low];
+        s[low] = s[high];
+        s[high] = temp;
+
+        low ++;
+        high --;
+    }
+
+    return s;
+};

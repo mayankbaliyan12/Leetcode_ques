52ff3c130a441c9dc682558061ce7b6216e539cf
mayankbaliyan12 <68837737+mayankbaliyan12@users.noreply.github.com>
2024-10-08 20:50:55 +0530

    Time: 49 ms (71.14%), Space: 49 MB (32.34%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    let result = '';
+    for (let i = 0; i < s.length; i++) {
+        const ascii = s.charCodeAt(i);
+        if (ascii >= 65 && ascii <= 90) {
+            result += String.fromCharCode(ascii + 32);
+        } else {
+            result += s.charAt(i);
+        }
+    }
+    return result;
+};

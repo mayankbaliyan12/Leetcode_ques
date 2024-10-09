68e17cc8f26387756559bbe88bdda8202da574ff
Mayank Baliyan <68837737+mayankbaliyan12@users.noreply.github.com>
2024-10-09 14:00:45 +0530

    Time: 51 ms (78.08%), Space: 49.6 MB (51.6%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.js
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.js
@@ -2,21 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-var minAddToMakeValid = function(s) {
-    let open = 0;  
-    let close = 0; 
-    
+var minAddToMakeValid = function (s) {
+    let open = 0, close = 0;
     for (let i = 0; i < s.length; i++) {
         if (s[i] === '(') {
-            open++;  
+            open++;
         } else if (s[i] === ')') {
             if (open > 0) {
-                open--;  
+                open--;
             } else {
-                close++;  
+                close++;
             }
         }
     }
-    
     return open + close;
 };

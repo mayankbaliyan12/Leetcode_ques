d89a0fcfad3aeb97764ca6f4fbed39cb80dd4d8d
Mayank Baliyan <68837737+mayankbaliyan12@users.noreply.github.com>
2025-01-17 14:44:15 +0530

    Time: 6 ms (13.65%), Space: 51.3 MB (25.25%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    const checked = [];
+    while (n !== 1) {
+        if (checked.includes(n)) return false;
+        checked.push(n);
+        n = n.toString().split('').map(i => i**2).reduce((sum, i) => sum + i, 0);
+    }
+    return true;
+};

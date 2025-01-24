d993e4a5c9875f59f1281ed7b07a3358c91f2906
Mayank Baliyan <68837737+mayankbaliyan12@users.noreply.github.com>
2025-01-25 00:51:39 +0530

    Time: 0 ms (100%), Space: 52.5 MB (81.21%) - LeetHub

---
+++ 0078-subsets/0078-subsets.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    let res = [[]]
+    for (let i = 0; i < nums.length; ++i) {
+  		let length = res.length;
+		for (let j = 0; j < length; ++j) {
+			res.push([...res[j],nums[i]])
+        }  
+   }
+   return res
+ };
